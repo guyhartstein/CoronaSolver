32313dee7a99cdedcfe69d8bfdfdd9e1412072af
Guy Hartstein <guyh2020@gmail.com>
2020-05-11 13:14:07 -0700

    Basic Setup

--- main.java
+++ main.java
@@ -1,6 +1,22 @@
+//import file reading
+import java.io.*;
+import java.util.*;
+
+public class CoronaSolver {
+
+  //Initialize output string
+  private String output = "";
+  private int totalDays;
+  private int flightExposed;
+  //File reading
+  private BufferedReader reader;
+  private File inputFileName = new File()
 
 
+  public String solve(String inputFileName){
+    reader = new BufferedReader(new FileReader(inputFileName));
 
-public String solve(){
+    //Instantiate the private instance variables using the FileReader
 
+  }
 }
